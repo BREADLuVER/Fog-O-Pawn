24890b8f562657932f0742e6e6ac7016d3bfdd7a
BREADLuVER <breadyyin@email.com>
2025-06-17 15:52:43 -0400

    Update FogOfPawn binaries and assembly information for version tracking. Adjusted sourcelink to reflect the latest commit for improved debugging and source mapping.

--- Source/obj/Debug/net48/FogOfPawn.AssemblyInfo.cs
+++ Source/obj/Debug/net48/FogOfPawn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FogOfPawn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62d902e5c9eed2fee56430d1c341dbc238ebab42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6e3e6a6034b878e8dc120ab0b278a4682b5719b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FogOfPawn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FogOfPawn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
